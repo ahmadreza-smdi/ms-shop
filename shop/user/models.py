8a390f7dbdc5ffb5efd6ed23650ed75d810000d3
Ahmadreza Samadi <ahmadreza.smdi@gmail.com>
2019-08-31 14:49:17 +0430

    Adding Customer model to user models and activate it in admin page

--- shop/user/models.py
+++ shop/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    sexuality = (
+        ('M','male'),
+        ('F','female'),
+        ('O','other'),
+    )
+    sex = models.CharField(max_length = 1 , choices = sexuality)
+    phone_number = models.CharField(max_length = 11)
+    birth_date = models.DateTimeField()
+    age = models.IntegerField()
+    address = models.CharField(max_length = 255)
+    national_code = models.CharField(max_length = 11)
+
+
